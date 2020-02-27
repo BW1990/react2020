66b4ff9c5ba682acebe9eff4a66885eda1f71528
BW1990 <brianwinward@hotmail.com>
2020-02-27 14:20:58 -0500

    Updates

---
+++ precache-manifest.c94cb922c4088b743e084d8ded591e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72151ce20324743cf75623a41ff446c9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d9d91f04a04db9fcc77f",
+    "url": "/monsters-rolodex/static/css/main.c8689ded.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d91f04a04db9fcc77f",
+    "url": "/monsters-rolodex/static/js/main.0c0421e9.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
